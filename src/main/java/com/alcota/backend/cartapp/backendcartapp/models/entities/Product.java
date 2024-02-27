5e9d36924a1dc4d5585b85fc14db661d0dad9d7c
nalcota <ni.alcota@duoucuc.cl>
2024-02-27 10:42:23 -0300

    Inicializando creando la clase Entity de Persistencia JPA

---
+++ src/main/java/com/alcota/backend/cartapp/backendcartapp/models/entities/Product.java
@@ -0,0 +1,60 @@
+package com.alcota.backend.cartapp.backendcartapp.models.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Para Mysql Incrementar id
+
+    private Long id;
+
+    //@Column(name = "name_product") en caso de que se llame distinto
+    private String name;
+
+    private String description;
+
+    private Long price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    
+
+}
